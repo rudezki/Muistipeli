e5a29f15e7a0b78315e2c53ee02feae8a1666e0c
Arttu Pylkkö <rudezki@gmail.com>
2020-04-06 12:47:52 +0300

    Initial commit

--- Muistipeli/Form1.cs
+++ Muistipeli/Form1.cs
@@ -6,15 +6,123 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Media;
 using System.Windows.Forms;
 
 namespace Muistipeli
 {
     public partial class Form1 : Form
     {
+        private SoundPlayer sound;
+
+        Random random = new Random();
+
+        Label firstClicked = null;
+
+        Label secondClicked = null;
+
+        List<string> icons = new List<string>()
+        {
+            "!", "!", "N", "N", ",", ",", "k", "k", "b", "b", "v", "v", "w", "w", "z", "z"
+        };
+
+        private void AssignIconsToSquares()
+        {
+            foreach (Control control in tableLayoutPanel1.Controls)
+            {
+                Label iconLabel = control as Label;
+                if (iconLabel != null)
+                {
+                    int randomNumber = random.Next(icons.Count);
+                    iconLabel.Text = icons[randomNumber];
+                    iconLabel.ForeColor = iconLabel.BackColor;
+                    icons.RemoveAt(randomNumber);
+                }
+            }
+        }
+        
+
         public Form1()
         {
             InitializeComponent();
+            AssignIconsToSquares();
+            sound = new SoundPlayer(@"c:\Windows\Media\ding.wav");
+        }
+
+        private void label_Click(object sender, EventArgs e)
+        {
+            if (timer1.Enabled == true)
+                return;
+            Label clickedLabel = sender as Label;
+
+            if (secondClicked != null)
+                return;
+
+            if (clickedLabel != null)
+            {
+                if (clickedLabel.ForeColor == Color.Black)
+                    return;
+
+                clickedLabel.ForeColor = Color.Black;
+
+                if (firstClicked == null)
+                {
+                    firstClicked = clickedLabel;
+                    firstClicked.ForeColor = Color.Black;
+
+                    return;
+                }
+                secondClicked = clickedLabel;
+                secondClicked.ForeColor = Color.Black;
+
+                CheckForWinner();
+
+                if (firstClicked.Text == secondClicked.Text)
+                {
+                    sound.Play();
+                    firstClicked.BackColor = Color.Green;
+                    secondClicked.BackColor = Color.Green;
+                    firstClicked = null;
+                    secondClicked = null;
+                    
+                    return;
+                }
+
+                timer1.Start();
+            }
+        }
+        private void CheckForWinner()
+        {
+            foreach (Control control in tableLayoutPanel1.Controls)
+            {
+                Label iconLabel = control as Label;
+                if (iconLabel != null)
+                {
+                    if (iconLabel.ForeColor == iconLabel.BackColor)
+                        return;
+                }
+            }
+            sound.Play();
+            firstClicked.BackColor = Color.Green;
+            secondClicked.BackColor = Color.Green;
+            MessageBox.Show("Winner winner chicken dinner!");
+            Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Stop();
+
+            firstClicked.ForeColor = firstClicked.BackColor;
+            secondClicked.ForeColor = secondClicked.BackColor;
+
+            firstClicked = null;
+            secondClicked = null;
         }
     }
 }
